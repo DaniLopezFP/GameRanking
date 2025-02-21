5cddec0a4318d7fb71867ae1f17c4dd06e670834
mercuriosas <dlopezefp@gmail.com>
2025-02-21 13:02:36 +0100

    UT2P1 - Finalizada

---
+++ app/src/main/java/com/example/gameranking_ut1/MainActivity.kt
@@ -0,0 +1,207 @@
+package com.example.gameranking_ut1
+
+import MenuNewPlayer
+import android.content.res.Configuration
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.scrollable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.gameranking_ut1.ui.theme.GameRanking_UT1Theme
+import com.example.gameranking_ut1.ui.theme.colorText
+import com.example.gameranking_ut1.ui.theme.courgetteRegularFamily
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            GameRanking_UT1Theme {
+                val navController = rememberNavController()
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Box(Modifier.padding(innerPadding)) {
+                        //MenuPlayGames()
+                       // MenuNewPlayer(modifier = Modifier.padding(innerPadding))
+                        NavHost(
+                            navController = navController,
+                            startDestination = "RankingGames"
+                        ) {
+                            composable("NewPlayer") { MenuNewPlayer() }
+                            composable("RankingGames") {MenuPlayGames(navController) }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun MenuPlayGames(navController: NavHostController) {
+    val configuration = LocalConfiguration.current
+
+    when (configuration.orientation) {
+        Configuration.ORIENTATION_PORTRAIT -> {
+            MenuVertical(navController)
+        }
+        else -> {
+            MenuHorizontal(navController)
+        }
+    }
+}
+
+@Composable
+fun MenuVertical(navController: NavHostController) {
+    Box(
+        modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center
+    ) {
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Box(
+                modifier = Modifier
+                    .background(Color.Cyan)
+                    .size(150.dp)
+            ) {
+
+            }
+            Text(
+                "Game Ranking", style = TextStyle(
+                    fontSize = 50.sp,
+                    fontFamily = courgetteRegularFamily,
+                    fontWeight = FontWeight.Normal,
+                    color = colorText
+                )
+            )
+
+            Button(
+                onClick = { }, modifier = Modifier.width(200.dp)
+            ) {
+                Text(text = "Ranking")
+            }
+            Button(
+                onClick = { navController.navigate("NewPlayer") },
+                modifier = Modifier.width(200.dp)
+            ) {
+                Text(text = "New Player")
+            }
+            Button(
+                onClick = { }, modifier = Modifier.width(200.dp)
+            ) {
+                Text(text = "Preferences")
+            }
+            Button(
+                onClick = { }, modifier = Modifier.width(200.dp)
+            ) {
+                Text(text = "Users")
+
+            }
+
+            Button(
+                onClick = { },//navController.navigate("About") },
+                modifier = Modifier.width(200.dp)
+            ) {
+                Text(text = "About")
+            }
+        }
+    }
+}
+
+
+@Composable
+fun MenuHorizontal(navController: NavHostController) {
+    Box(
+        modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center
+    ) {
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally, modifier = Modifier.verticalScroll(
+                rememberScrollState()
+            )
+        ) {
+            Row {
+                Box(
+                    modifier = Modifier
+                        .background(Color.Cyan)
+                        .size(150.dp)
+                ) {
+                }
+            }
+
+            Row {
+                Text(
+                    "Game Ranking", style = TextStyle(
+                        fontSize = 50.sp,
+                        fontFamily = courgetteRegularFamily,
+                        fontWeight = FontWeight.Normal,
+                        color =  colorText
+                    )
+                )
+            }
+            Row {
+
+                Button(
+                    onClick = { navController.navigate("NewPlayer") },
+                    modifier = Modifier.width(200.dp)
+                ) {
+                    Text(text = "New Player")
+                }
+                Button(
+                    onClick = { }, modifier = Modifier.width(200.dp)
+                ) {
+                    Text(text = "Ranking")
+                }
+                Button(
+                    onClick = { }, modifier = Modifier.width(200.dp)
+                ) {
+                    Text(text = "Preferences")
+                }
+            }
+            Row {
+                Button(
+                    onClick = { }, modifier = Modifier.width(200.dp)
+                ) {
+                    Text(text = "Users")
+
+                }
+                Button(
+                    onClick = { },//navController.navigate("About") },
+                    modifier = Modifier.width(200.dp)
+                ) {
+                    Text(text = "About")
+                }
+            }
+
+        }
+    }
+}
---
+++ app/src/main/java/com/example/gameranking_ut1/NewPlayer.kt
@@ -0,0 +1,196 @@
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.semantics.Role.Companion.Image
+import androidx.compose.ui.unit.dp
+import com.example.gameranking_ut1.R
+import com.example.gameranking_ut1.ui.theme.colorDani
+import com.example.gameranking_ut1.ui.theme.colorText
+import com.example.gameranking_ut1.ui.theme.inversePrimaryLight
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MenuNewPlayer(modifier: Modifier = Modifier) {
+    var EstadoNombre by remember { mutableStateOf("") }
+     var EstadoApellido by remember { mutableStateOf("") }
+     var EstadoNick by remember { mutableStateOf("") }
+     var EstadoTelf by remember { mutableStateOf("") }
+     var EstadoMail by remember { mutableStateOf("") }
+     var nameError by remember { mutableStateOf(false) } // Variable para el control de errores
+    var pesoH = 3f
+    var pesoV = 1f
+    var EstadoOutlined by remember { mutableStateOf("") }
+
+    //texto = "New Player"
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(top = 20.dp),
+        contentAlignment = Alignment.TopStart,
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                //.padding(paddingValues)
+                .verticalScroll(rememberScrollState()),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            //verticalArrangement = Arrangement.Center
+        ) {
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                Image(
+                    painter = painterResource(R.drawable.account),
+                    contentDescription = "User",
+                    Modifier
+                        .size(60.dp)
+                        .weight(1f),
+                    colorFilter = ColorFilter.tint(colorText)
+                )
+
+                OutlinedTextField(
+                    modifier = Modifier.weight(2f),
+                    value = EstadoOutlined, onValueChange = { EstadoOutlined = it },
+                    label = { Text(text = "First Name") },
+                    colors = OutlinedTextFieldDefaults.colors(
+                        focusedContainerColor = Color.Cyan,
+                        unfocusedContainerColor = Color.Cyan
+                    )
+                )
+            }
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                Spacer(Modifier.weight(1f))
+                OutlinedTextField(
+                    modifier = Modifier.weight(2f),
+                    value = EstadoOutlined, onValueChange = { EstadoOutlined = it },
+                    label = { Text(text = "Last Name") },
+                    colors = OutlinedTextFieldDefaults.colors(
+                        focusedContainerColor = Color.Cyan,
+                        unfocusedContainerColor = Color.Cyan
+                    )
+                )
+            }
+            Row(verticalAlignment = Alignment.CenterVertically) {
+                Image(
+                    painter = painterResource(R.drawable.mask),
+                    contentDescription = "User",
+                    Modifier
+                        .size(60.dp)
+                        .weight(1f),
+                    colorFilter = ColorFilter.tint(colorText)
+                )
+                OutlinedTextField(
+                    modifier = Modifier.weight(2f),
+                    value = EstadoOutlined, onValueChange = { EstadoOutlined = it },
+                    label = { Text(text = "NickName") },
+                    colors = OutlinedTextFieldDefaults.colors(
+                        focusedContainerColor = Color.Cyan,
+                        unfocusedContainerColor = Color.Cyan
+                    )
+                )
+            }
+            Spacer(Modifier.size(20.dp))
+            Row {
+
+                Image(
+                    painter = painterResource(R.drawable.android),
+                    contentDescription = "Android",
+                    Modifier
+                        .size(100.dp)
+                        .weight(2f)
+                )
+                Button(
+                    onClick = { },//navController.navigate("NewPlayer") },
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = colorDani,
+                        contentColor = Color.Black
+                    ),
+                    modifier = Modifier
+                        .width(200.dp)
+                        .weight(1f)
+                        .padding(vertical = 30.dp)
+                ) {
+                    Text(text = "Preferences")
+                }
+            }
+            Spacer(modifier = Modifier.size(20.dp))
+            Row(Modifier.fillMaxSize()) {
+                Image(
+                    painter = painterResource(R.drawable.phone),
+                    contentDescription = "Telefono",
+                    Modifier
+                        .size(60.dp)
+                        .weight(1f),
+                    colorFilter = ColorFilter.tint(colorText)
+                )
+
+                TextField(
+                    value = EstadoTelf,
+                    onValueChange = {
+                        EstadoTelf = it
+                    },
+                    Modifier.weight(2f),
+                    colors = TextFieldDefaults.textFieldColors(
+                        containerColor = Color.Cyan,
+                        focusedIndicatorColor = colorDani
+                    ),
+                    label = { Text(text = ("Teléfono")) },
+                    shape = RoundedCornerShape(topStart = 20.dp, topEnd = 20.dp)
+                )
+            }
+            Spacer(modifier = Modifier.size(20.dp))
+            Row(Modifier.fillMaxSize()) {
+                Image(
+                    painter = painterResource(R.drawable.email),
+                    contentDescription = "E-mail",
+                    Modifier
+                        .size(60.dp)
+                        .weight(1f),
+                    colorFilter = ColorFilter.tint(colorText)
+                )
+                TextField(
+                    value = EstadoMail,
+                    onValueChange = {
+                        EstadoMail = it
+                    },
+                    Modifier.weight(2f),
+                    colors = TextFieldDefaults.textFieldColors(
+                        containerColor = Color.Cyan,
+                        focusedIndicatorColor = colorDani
+                    ),
+                    label = { Text(text = ("E-mail")) },
+                    shape = RoundedCornerShape(topStart = 20.dp, topEnd = 20.dp)
+                )
+            }
+        }
+
+    }
+}
